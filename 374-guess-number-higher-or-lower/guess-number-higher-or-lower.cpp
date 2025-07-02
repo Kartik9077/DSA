d1bb2a01b9f661cd89c2d0c8c93920c730dd8f50
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-07-02 13:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.55%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -13,7 +13,7 @@ public:
     __int128 l=1,r=n;
     while(l<=r)
     {
-        __int128 mid=(l+r)/2;
+        __int128 mid=(l+r+1)/2;
         if(guess(mid)==0)return mid;
         if(guess(mid)==-1)r=mid-1;
         else l=mid+1;
