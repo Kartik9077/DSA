2ed6432dbf8073f8bc60cb0950bce20036eae8b8
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-07-12 09:30:11 +0530

    Time: 2 ms (51.51%) | Memory: 7.9 MB (34.60%) - LeetSync

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int f=0;
+        while(a!=0||b!=0||c!=0){
+            int a_bit=a&1,b_bit=b&1,c_bit=c&1;
+            if((a_bit|b_bit)!=c_bit){
+                if(c_bit==1)f+=1;
+                else f+=(a_bit+b_bit);
+            }
+        
+        a>>=1;
+        b>>=1;c>>=1;}
+        return f;
+        
+    }
+};
