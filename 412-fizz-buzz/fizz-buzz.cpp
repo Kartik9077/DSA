dba20425aaf24187ef0e636a1d1c9ebf33654f7d
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-07-15 11:13:05 +0530

    Time: 3 ms (29.97%) | Memory: 11.9 MB (15.78%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -4,9 +4,10 @@ public:
     vector<string>ans;
     for(int i=1;i<=n;i++){
         if(i%3==0&&i%5==0){ans.push_back("FizzBuzz");continue;}
-        if(i%3==0){ans.push_back("Fizz");continue;}
-        if(i%5==0)ans.push_back("Buzz");
         if(i%3!=0&&i%5!=0)ans.push_back(to_string(i));
+        if(i%3==0){ans.push_back("Fizz");}
+        if(i%5==0)ans.push_back("Buzz");
+       
         
     
     }
