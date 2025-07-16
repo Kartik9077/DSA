34dba43c1420af1ca3a602d93338e412000e12c8
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-07-16 09:11:09 +0530

    Time: 14 ms (27.06%) | Memory: 96.1 MB (62.35%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int c=nums[0]%2,odd=0,even=0,both=0;
+        for(auto it:nums){
+            if(it%2==0)even++;
+            else odd++;
+            if(it%2==c){both++;c=1-c;}
+        }
+        return max(both,max(odd,even));
+    }
+};
