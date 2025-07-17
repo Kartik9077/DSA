45daa08440565964146a622bbc53ba4fb007acbb
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-07-17 07:23:11 +0530

    Time: 91 ms (95.28%) | Memory: 130.6 MB (40.41%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -4,8 +4,8 @@ public:
         vector<vector<int>>dp(k,vector<int>(k,0));
         int maxlen=0;
         for(auto num:nums){
+            int rem=num%k;
             for(int j=0;j<k;j++){
-                int rem=num%k;
                 maxlen=max(maxlen,dp[rem][j]=dp[j][rem]+1);
 
             }
