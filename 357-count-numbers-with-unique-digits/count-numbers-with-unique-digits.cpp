cf92c5f8ab7afa9d496e459c8efe12109cfb03c0
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-01 23:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (72.77%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
   int solve(int n,int x,int y,int pos){
+    if(n==0)return 1;
+    if(n==1)return 10;
       if(pos>n)return x;
      y*=(10-(pos-1));
      x+=y;
@@ -8,17 +10,7 @@ public:
       
   }
     int countNumbersWithUniqueDigits(int n) {
-        // if(n==0)return 1;
-        // if(n==1)return 10;
-        // int to=10;
-        // int curr=9;
-        // for(int i=2;i<=n;i++){
-        // curr *= (10 - (i - 1));
-        // to+=curr;
-        // }
-        // return to;
-         if(n==0)return 1;
-    if(n==1)return 10;
+       
         return solve(n,10,9,2);
         
     }
