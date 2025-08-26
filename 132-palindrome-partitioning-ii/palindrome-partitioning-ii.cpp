4d4e6327d54c89e218dcb5ab054042be4b44c0ad
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-26 18:09:15 +0530

    Time: 7 ms (99.28%) | Memory: 9.2 MB (85.59%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void helper(int l, int r, string& s, vector<int>& dp) {
+        int n = s.size();
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            if (l == 0)
+                dp[r] = 0;
+            else
+                dp[r] = min(dp[r], dp[l - 1] + 1);
+            l--;
+            r++;
+        }
+    }
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; i++) {
+            helper(i, i, s, dp);     
+            helper(i, i + 1, s, dp);
+        }
+        return dp[n - 1];
+    }
+};
