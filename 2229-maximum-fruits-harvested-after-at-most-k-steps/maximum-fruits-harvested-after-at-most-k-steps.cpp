49c6f0b622d3e9635515226745620b36ff384609
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-26 19:11:33 +0530

    Update maximum-fruits-harvested-after-at-most-k-steps.cpp

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -3,7 +3,7 @@ public:
     int maxTotalFruits(vector<vector<int>>& fruits, int pos, int k) {
         auto l = upper_bound(begin(fruits), end(fruits), vector<int>{pos - k});
     int sum = 0, max_sum = 0;
-    for (auto r = l; r != end(fruits) && (r)[0] <= pos + k; ++r) {
+    for (auto r = l; r != end(fruits) && (*r)[0] <= pos + k; ++r) {
         sum += (*r)[1];
         while(min(pos - 2 * (*l)[0] + (*r)[0], 2 * (*r)[0] - (*l)[0] - pos) > k)
             sum -= (*l++)[1];
