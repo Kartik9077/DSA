7efa372c06ded7700bac8f1f01be2c33f133d762
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-27 13:18:11 +0530

    Update checking-existence-of-edge-length-limited-paths.cpp

--- 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.cpp
+++ 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.cpp
@@ -23,9 +23,7 @@ public:
     }
     }
     vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList,vector<vector<int>>& queries) {
-        
         parent.resize(n);
-        
         rank.resize(n);
         for (int i = 0; i < n; i++) {
             parent[i] = i;
