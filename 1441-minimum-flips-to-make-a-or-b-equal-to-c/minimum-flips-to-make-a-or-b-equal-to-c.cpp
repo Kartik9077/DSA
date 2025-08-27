febc94455571bbd73981538c69d342b396c3e61c
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-27 13:30:54 +0530

    Update minimum-flips-to-make-a-or-b-equal-to-c.cpp

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,7 +4,7 @@ public:
         int f=0;
         while(a!=0||b!=0||c!=0){
             int a_bit=a&1,b_bit=b&1,c_bit=c&1; 
-            if((a_bit|b_bit)!=c_bit){ 
+            if((a_bit|b_bit)!=c_bit){  
                 if(c_bit==1)f+=1;  
                 else f+=(a_bit+b_bit);  
             }   
