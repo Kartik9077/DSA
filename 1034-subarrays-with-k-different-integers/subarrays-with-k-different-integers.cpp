89ef39295a365ecaa38bd75b25346a14ef0cbfc1
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-27 13:22:59 +0530

    Update subarrays-with-k-different-integers.cpp

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -8,7 +8,7 @@ int atmostK(vector<int>& nums, int k){
         unordered_map<int,int>m;   
         while(r<n){
             m[nums[r]]++;
-            while(m.size()>k){
+            while(m.size()>k){ 
                 m[nums[l]]--;
                 if(m[nums[l]]==0){ 
                     m.erase(nums[l]);
