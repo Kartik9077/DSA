b3201a89a75a7955b083374c36347f043ff6c6c9
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-28 20:10:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (48.82%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int i=1;
+        int count=0;
+        while(i<=min(a,b)){
+            if(a%i==0&&b%i==0)count++;
+            i++;
+        }
+        return count;
+    }
+};
