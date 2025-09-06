d72b7d8522226044a88b19889a434ca20fc100e7
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-09-06 13:30:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (86.87%) - LeetSync

---
+++ 1148-adding-two-negabinary-numbers/adding-two-negabinary-numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+typedef long long ll;
+
+    vector<int> addNegabinary(vector<int>& arr1, vector<int>& arr2) {
+         ll i = arr1.size() - 1, j = arr2.size() - 1;
+         ll carry = 0;
+        vector<int> res;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            ll x = (i >= 0 ? arr1[i--] : 0);
+            ll y = (j >= 0 ? arr2[j--] : 0);
+
+            ll s = x + y + carry;
+            ll digit ;
+            if(x+y==0 && carry==-1){digit=1;carry=1;}
+            else{digit=s%2;carry=-s/2;
+            }
+            res.push_back(digit);
+        }
+        while (res.size() > 1 && res.back() == 0) res.pop_back();
+        reverse(res.begin(), res.end());
+        return res;
+        
+         
+    }
+};
