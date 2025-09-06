576f7a700e6bcd2d0d06c33e2dcd3404f45e4f7c
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-09-06 14:45:18 +0530

    Time: 24 ms (61.08%) | Memory: 41.7 MB (77.65%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
-     map<int,int>mpp;
+     unordered_map<int,int>mpp;
         for(auto ti:nums){
             if(ti&1)continue;
             else mpp[ti]++;
         }
         int ans=-1,maxii=0;
         for(auto it:mpp){
-            if(it.second>maxii){
+            if(it.second>=maxii){
+                if(it.second>maxii){
                 maxii=it.second;
-                ans=it.first;
+                ans=it.first;}
+                else if(ans>it.first)ans=it.first;
             }
         }
         return ans;
