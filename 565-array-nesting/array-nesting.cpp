87875857cbd73888eff3a864d8570c4b93811aee
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-09-08 21:29:04 +0530

    Time: 343 ms (5.07%) | Memory: 150.5 MB (9.76%) - LeetSync

---
+++ 565-array-nesting/array-nesting.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+       int ans=0;set<int>st;
+        for(int a=0;a<nums.size();a++){
+            if(st.find(a)!=st.end())continue; 
+            int c=0;
+        while(st.find(nums[a])==st.end()){
+            c+=1;
+            st.insert(nums[a]);
+            a=nums[a];
+        }
+        ans=max(ans,c);
+        }
+        return ans;
+    }
+};
