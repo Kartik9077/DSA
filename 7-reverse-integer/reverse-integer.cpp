1ed3ad07792a2f9338b6ec967bb3aa8200832e44
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-09-14 03:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (53.75%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -3,8 +3,9 @@ public:
     int reverse(int x) {
         __int128 rev=0;
         while(x!=0){
+            int k=x%10;
             rev*=10;
-            rev+=x%10;
+            rev+=k;
             x/=10;
         }
         if(x<0)rev*=-1;
