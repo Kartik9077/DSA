d4d5010e82deaa28d2cdce694e5da86b0c46a6bd
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-10-25 08:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.15%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int totalMoney(int n) {
-      int k=n/7;int p=28;
-      int maxi=k,ans=0;
+      int k=n/7,p=28,maxi=k,ans=0;n%=7;
       while(k--){
         ans+=p;p+=7;
       }
-      n%=7;
       while(n--){
         ans+=maxi+1;
         maxi++;
