8e698a27338c4999aecfdfb031e254bf8f63a0f8
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-11-10 20:57:53 +0530

    Time: 24 ms (81.74%) | Memory: 222 MB (87.36%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> nse(n, n);
-        unordered_map<int, set<int>> map;
-
-        int upperBound = 0;
-        for (int i = 0; i < n; i++) {
-            map[nums[i]].insert(i);
-        }
-
-        stack<int> st;
-        st.push(n - 1);
-
-        
-        for (int i = n - 2; i >= 0; i--) {
-            while (!st.empty() && nums[i] <= nums[st.top()]) st.pop();
-            if (!st.empty()) {
-                nse[i] = st.top();
+        stack<int>st;int ans=0;
+        for(auto it:nums){
+            while(!st.empty()&&st.top()>it){
+                st.pop();
+                ans++;
             }
-            st.push(i);
+            if(it>0 && (st.empty()||st.top()<it))st.push(it);
+
         }
-
-        
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) continue;
-            int j = nse[i];
-            count++;
-            
-            vector<int> toErase;
-
-            for (int curr : map[nums[i]]) {
-                if (curr > j) break;
-                toErase.push_back(curr);
-            }
-            for (int curr : toErase) {
-                map[nums[i]].erase(curr);
-                nums[curr] = 0;
-            }
+        while(!st.empty()){
+            st.pop();
+            ans++;
         }
-
-        return count;
+        return ans;
     }
 };
